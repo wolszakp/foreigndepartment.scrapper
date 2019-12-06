ede94db8375eb80cd0bd48e6aa3c7707408012af
Piotr Wolszakiewicz <wolszakp@gmail.com>
2019-12-06 15:03:04 +0100

    - upgrade projects to netstandard2.1 + netcoreapp3.1
    - Add draft version of Howtosavemoney.Scrapper

--- ForeignCountry.Console/Program.cs
+++ ForeignCountry.Console/Program.cs
@@ -4,20 +4,32 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Howtosavemoney.Scrapper;
 
 namespace ForeignCountry.Console
 {
     class Program
     {
+        // outDirectory
+        // verbose
+        // jakoszczędzać + dziennik zagraniczny
+        // Force refresh
+
         private static HttpClient HttpClient = new HttpClient();
-        private static string OutputPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        private static string OutputPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "wnop");
         private static string PageLinksFileName = Path.Combine(OutputPath, "pageLinks.txt");
         private static string Mp3LinksFileName = Path.Combine(OutputPath, "mp3Links.txt");
         private static string OutputMp3Path = Path.Combine(OutputPath, "out");
 
         static void Main(string[] args)
         {
-            var mainUrl = @"https://www.dzialzagraniczny.pl/";
+            if(!Directory.Exists(OutputPath))
+            {
+                Directory.CreateDirectory(OutputPath);
+            }
+            // var mainUrl = @"https://www.dzialzagraniczny.pl/";
+            var mainUrl = @"https://jakoszczedzacpieniadze.pl/podcast";
+
             MainAsync(mainUrl).GetAwaiter().GetResult();            
         }
 
@@ -36,11 +48,15 @@ namespace ForeignCountry.Console
         {
             if(File.Exists(Mp3LinksFileName)) 
             {
+                System.Console.WriteLine($"Download list cache exists: {Mp3LinksFileName}");
                 return await File.ReadAllLinesAsync(Mp3LinksFileName);
             }
 
-            var seleniumScrapper = new Selenium.Scrapper.SeleniumScrapper();
-            var downloadUrls = seleniumScrapper.GetDownloadUrlsFromArticlePages(podcastPagesUrls);            
+            //var seleniumScrapper = new Selenium.Scrapper.SeleniumScrapper();
+            //var downloadUrls = seleniumScrapper.GetDownloadUrlsFromArticlePages(podcastPagesUrls);            
+            var scrapper = new Scrapper();
+            var downloadUrls = await scrapper.GetDownloadUrlsFromArticlePages(podcastPagesUrls);
+
             await File.WriteAllLinesAsync(Mp3LinksFileName, downloadUrls);
             return downloadUrls;
         }
@@ -49,11 +65,14 @@ namespace ForeignCountry.Console
         {
             if(File.Exists(PageLinksFileName)) 
             {
+                System.Console.WriteLine($"Article urls cache exists: {PageLinksFileName}");
                 return await File.ReadAllLinesAsync(PageLinksFileName);
             }
 
-            var seleniumScrapper = new Selenium.Scrapper.SeleniumScrapper();
-            var podcastPagesUrls = seleniumScrapper.ReadAllPodcastLinks(url);            
+            // var seleniumScrapper = new Selenium.Scrapper.SeleniumScrapper();
+            // var podcastPagesUrls = seleniumScrapper.ReadAllPodcastLinks(url);
+            var scrapper = new Scrapper();
+            var podcastPagesUrls = await scrapper.ReadAllPodcastLinks(url);
             await File.WriteAllLinesAsync(PageLinksFileName, podcastPagesUrls);
             return podcastPagesUrls;
         }
@@ -61,6 +80,12 @@ namespace ForeignCountry.Console
         static async Task DownloadMp3File(string url) 
         {
             var fileName = url.Split("/", StringSplitOptions.RemoveEmptyEntries).Last();
+            var filePath = Path.Combine(OutputMp3Path, fileName);
+            if(File.Exists(filePath))
+            {
+                System.Console.WriteLine($"File ${fileName} already exists.");
+                return;
+            }
 
             var uri = new Uri(url);
             var response = await HttpClient.GetAsync(uri);
@@ -68,8 +93,8 @@ namespace ForeignCountry.Console
             {
                 Directory.CreateDirectory(OutputMp3Path);
             }
-            
-            using (var fs = new FileStream(Path.Combine(OutputMp3Path, fileName), FileMode.CreateNew))
+
+            using (var fs = new FileStream(filePath, FileMode.CreateNew))
             {
                 await response.Content.CopyToAsync(fs);
                 System.Console.ForegroundColor = ConsoleColor.Green;
---
+++ Howtosavemoney.Scrapper/Scrapper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+
+namespace Howtosavemoney.Scrapper
+{
+    public class Scrapper
+    {
+        private readonly IBrowsingContext _context;
+
+        public Scrapper()
+        {
+            var configuration = Configuration.Default
+                .WithDefaultLoader();
+            _context = BrowsingContext.New(configuration);
+        }
+
+        public async Task<IEnumerable<string>> GetDownloadUrlsFromArticlePages(IEnumerable<string> articleUrls)
+        {
+            var downloadUrls = new HashSet<string>();
+            foreach(var item in articleUrls) 
+            {
+                var downloadUrl = await GetDownloadUrlFromArticlePage(item);
+                if( downloadUrl != null) 
+                {
+                    downloadUrls.Add(downloadUrl);
+                    continue;
+                
+                }
+                
+                System.Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Download url doesn't exist for: {item}");
+                Console.ResetColor();
+            }
+            return downloadUrls;
+        }
+
+        public async Task<string> GetDownloadUrlFromArticlePage(string url)
+        {
+            var document = await _context.OpenAsync(url);
+            var mp3Selector = "p.powerpress_links.powerpress_links_mp3 > a.powerpress_link_d";
+            var link = document.QuerySelector(mp3Selector);
+            return link?.GetAttribute("href");
+        }
+
+        public async Task<IEnumerable<string>> ReadAllPodcastLinks(string url)
+        {
+            var urls = new List<string>();
+            var visitedUrls = new List<string>();
+            var nextUrl = url;
+            do
+            {
+                visitedUrls.Add(nextUrl);
+                var document = await _context.OpenAsync(nextUrl);
+                urls.AddRange(ReadPageLinks(document));
+
+                var nextPageSelector = "p.pagination > a";
+                var nextPage = document.QuerySelectorAll(nextPageSelector);
+                nextUrl = nextPage?.SkipWhile(n => n.ClassName != "current").Skip(1).FirstOrDefault()?.GetAttribute("href");
+            } while (!string.IsNullOrEmpty(nextUrl) && !visitedUrls.Contains(nextUrl));
+
+            return urls.Distinct();
+        }
+
+        public IEnumerable<string> ReadPageLinks(IDocument document)
+        {
+            var hrefsSelector = "div.teaser > h2.entry-title > a";
+            var hrefs = document.QuerySelectorAll(hrefsSelector);
+
+            var urls = hrefs.Select(m => m.GetAttribute("href"));
+            return urls;
+        }
+    }
+}
